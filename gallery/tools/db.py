d022e9d453c8632d9752ed2192b43c16189fdae0
EC2 Default User <ec2-user@ip-172-31-34-179.us-east-2.compute.internal>
2020-06-08 03:59:01 +0000

    Created db.py

---
+++ gallery/tools/db.py
@@ -0,0 +1,34 @@
+import psycopg2
+
+db_host ="database-1.cgh7vkgen2ke.us-east-2.rds.amazonaws.com"
+db_name="image_gallery"
+db_user="image_gallery"
+
+password_file = "/home/ec2-user/.image_gallery_config"
+
+connection = None
+
+def get_password():
+        f = open(password_file, "r")
+        result = f.readline()
+        f.close()
+        return result[:-1]
+
+def connect():
+	global connection
+	connection = psycopg2.connect(host=db_host, dbname=db_name, user=db_user, password=get_password())
+
+def execute(query):
+        global connection
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor
+
+def main():
+	connect()
+	res = execute('select * from users')
+	for row in res:
+                print(row)
+
+if __name__ == '__main__':
+        main()
