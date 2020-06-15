182374f2467c2cf6926441c384cec20fef4a272e
EC2 Default User <ec2-user@ip-10-0-1-59.us-east-2.compute.internal>
2020-06-15 00:30:35 +0000

    adding ui flask functionality

--- gallery/ui/app.py
+++ gallery/ui/app.py
@@ -2,11 +2,19 @@ from flask import Flask
 from flask import request
 from flask import render_template
 
+from db import all_users
+from db import all_usernames
+from db import insertUser
+from db import editUser
+from db import deleteUser
+
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!, how are you this evening?'
+    y = 1
+    return 'Hello, AGAIN World!, how are you this evening?'
 
 @app.route('/goodbye')
 def goodbye():
@@ -15,3 +23,14 @@ def goodbye():
 @app.route('/greet/<name>')
 def greet(name):
     return 'nice to meet you ' + name
+
+
+@app.route('/admin')
+def user_list():
+     name1 = 'testies'
+     names = all_usernames() 
+     return render_template("users.html", name=names[0])
+
+@app.route('/admin/adduser')
+def add_new_user():
+    return render_template("new_user.html")
